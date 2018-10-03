3cf406e0edfb31c6684b5eb1c158485e554b00cc
Kirby <rosekatkirby@gmail.com>
2018-10-02 20:00:04 -0700

    Added User Table on Admin Page

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,3 +1,9 @@
 $(document).ready(() => {
     console.log("Ready!");
+    $.get("/users", null, null, "json").done((users) =>{
+        for (var i = 0; i < users.length; i++) {
+            console.log(users[i])
+            $('#userTable > tbody:last-child').append('<tr><td>' + users[i].username + '</td</tr>');
+        }
+    })
 });
